84d1082c11ea0134650d837c9ff703c9b7dbc8b6
Zack Bostian <bostianz@gmail.com>
2016-02-18 08:26:57 -0500

    need to add Router, but added an extra get route to find by ID

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var morgan = require('morgan');
 
 // Define our Todo Model
 var Todo = mongoose.model('Todo', {
@@ -15,13 +17,21 @@ mongoose.connect('mongodb://localhost/todo');
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors());
+
+//Logging
+app.use(morgan('combined'));
+
+//var todosRouter = express.Router();
+
+
 
 // // Routes
 
 //GET TODOS
-app.get('/todo', function (req, res) {
+app.get('/todo/:todo_id', function(req, res) {
 
-    Todo.find(function (err, todo) {
+    Todo.findById(req.params.todo_id, function (err, todo) {
 
         if(err) {
           return  res.send(err)
@@ -31,9 +41,18 @@ app.get('/todo', function (req, res) {
     });
 });
 
+app.get('/todo', function(req, res) {
+  Todo.find(function(err, todo) {
+      if(err) {
+        return res.send(err)
+      }
+    return res.json(todo);
+  });
+});
+
 //POST TODOS
 app.post('/todo', function(req, res) {
-
+  console.log('This is the post ' + JSON.stringify(req.body));
     Todo.create({
 
       text: req.body.text
@@ -43,12 +62,13 @@ app.post('/todo', function(req, res) {
         if(err) {
           return res.send(err);
         }
+      return res.json(todo);
     });
 });
 
 //DELETE TODOS
 app.delete('/todo/:todo_id', function(req, res) {
-
+  console.log(JSON.stringify(req.params.todo_id));
     Todo.remove({
 
         _id: req.params.todo_id
@@ -60,6 +80,7 @@ app.delete('/todo/:todo_id', function(req, res) {
         }
     });
 });
+
 
 app.put('/todo', function (req, res) {
   res.send('PUT Todo');
