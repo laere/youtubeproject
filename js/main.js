abc3b5171a98651ebb5f4abca58eb94954b699b7
Zack Bostian <bostianz@gmail.com>
2016-03-07 19:16:53 -0500

    added files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+window.onload = function() {
+
+  var $title = $('a');
+
+  $title.click(function() {
+
+    var currTitle = $('a.selected');
+    // remove class from curr target
+    currTitle.removeClass('selected');
+    // add class to target clicked
+    $(this).addClass('selected');
+    //hide other movies by default
+    $('div.mainContent').hide()
+    //show movie based on href attribute (which is an id of the title of the movie)
+    $(this.getAttribute('href')).show();
+
+  });
+
+}
