579b3a0045bf0be3ea192739a6f1149c9e830bd5
Zack Bostian <bostianz@gmail.com>
2016-03-08 18:41:18 -0500

    split up stop video snippet into own function

--- js/main.js
+++ js/main.js
@@ -4,22 +4,30 @@ $(document).ready(function() {
   var $title = $('a');
 
   $title.click(function() {
-
+    //hide all content other than one with visible class
+    $('div.mainContent').hide()
+    //currently selected title
     var currTitle = $('a.selected');
     // remove class from curr target
     currTitle.removeClass('selected');
     // add class to target clicked
     $(this).addClass('selected');
-    //hide other movies by default
-    $('div.mainContent').hide()
     //show movie based on href attribute (which is an id of the title of the movie)
     $(this.getAttribute('href')).show();
 
-    
-  	$('iframe').each(function(){
+    stopVideoOnSwitch();
+
+  });
+
+  function stopVideoOnSwitch() {
+
+    $('iframe').each(function() {
   		$(this)[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
   		console.log($(this)[0]);
   	});
-  });
+    
+  }
+
+
 
 });
