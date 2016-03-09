e4983ffac95e5e13c7dc3d7ba50d3fddaa220ec8
Zack Bostian <bostianz@gmail.com>
2016-03-08 19:19:29 -0500

    added fullscreen ability

--- js/main.js
+++ js/main.js
@@ -2,10 +2,21 @@
 $(document).ready(function() {
 
   var $title = $('a');
+  var $iframe =$('iframe');
+
+  $('button').click(function(){
+    // Play video and go fullscreen
+    console.log('hi');
+    var playerElement = $('.playerWrapper');
+    var requestFullScreen = playerElement.requestFullScreen || playerElement.mozRequestFullScreen || playerElement.webkitRequestFullScreen;
+    if (requestFullScreen) {
+      requestFullScreen.bind(playerElement)();
+    }
+  });
 
   $title.click(function() {
     //hide all content other than one with visible class
-    $('div.mainContent').hide()
+    $('div.mainContent').hide();
     //currently selected title
     var currTitle = $('a.selected');
     // remove class from curr target
@@ -21,13 +32,14 @@ $(document).ready(function() {
 
   function stopVideoOnSwitch() {
     //for each iframe element
-    $('iframe').each(function() {
+    $iframe.each(function() {
       //use the post message api to message the youtube api and retrieve the stopVideo func.
-  		$(this)[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
-  		console.log($(this)[0]);
-  	});
+      $(this)[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+      console.log($(this)[0]);
+    });
 
   }
+
 
 
 });
